3c06c2ff5809e9e748920cd85b1ba30f0ea29f35
DUMAS <you@example.com>
2020-01-16 16:02:00 +0100

    TP1 Spring V 1.0

---
+++ restaurant-console-app-tp-01/src/main/java/dev/App.java
@@ -0,0 +1,30 @@
+package dev;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import dev.ihm.Menu;
+import dev.service.PlatServiceVersion1;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext ("application-config.xml");
+        Scanner scanner = context.getBean(Scanner.class);
+        PlatServiceVersion1 plat = context.getBean(PlatServiceVersion1.class);
+
+        Menu menu = context.getBean(Menu.class);
+        
+        // récupération du bean Menu 
+        menu.afficher();
+        
+        // fermeture du Scanner        
+        context.getBean(Scanner.class).close();
+        
+        // fermeture du contexte Spring  
+
+        context.close();
+    }
+}
